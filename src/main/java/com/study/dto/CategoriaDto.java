7f4e6b16fcb97ea644a96ffe04ba2d8b160ad830
Joao Vale <jcarlos.vale@gmail.com>
2022-05-25 22:26:20 +0200

    dto testes.

--- src/main/java/com/study/dto/CategoriaDto.java
+++ src/main/java/com/study/dto/CategoriaDto.java
@@ -4,11 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
@@ -17,6 +15,6 @@ public class CategoriaDto {
 
     private Long id;
 
-    @NotBlank
+    @NotBlank(message="Nao pode ser blank")
     private String description;
 }
---
+++ src/test/java/com/study/dto/CategoriaDtoTest.java
@@ -0,0 +1,63 @@
+package com.study.dto;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CategoriaDtoTest {
+
+    @Test
+    void testarConstrutor() {
+        //GIVEN
+        Long id = 1L;
+        String description = "some description";
+
+        //WHEN
+        var dto = new CategoriaDto(id, description);
+
+        //THEN
+        assertThat(dto.getId()).isEqualTo(id);
+        assertThat(dto.getDescription()).isEqualTo(description);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidFields")
+    void testarConstrutorInvalido(final long id, final String description) {
+        //GIVEN
+        final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        final Validator validator = factory.getValidator();
+
+        //WHEN
+        var dto = new CategoriaDto(id, description);
+
+        //THEN
+        Set<ConstraintViolation<CategoriaDto>> violations = validator.validate(dto);
+
+        assertThat(violations).isNotEmpty();
+        assertThat(violations.size()).isEqualTo(1);
+        assertThat(
+                violations.stream()
+                .findFirst().get().getMessage()).isEqualTo("Nao pode ser blank");
+    }
+
+    private static Stream<Arguments> invalidFields() {
+        return Stream.of(
+                Arguments.arguments(1L, null),
+                Arguments.arguments(1L, ""),
+                Arguments.arguments(1L, " "),
+                Arguments.arguments(1L, "    "),
+                Arguments.arguments(1L, "      ")
+        );
+    }
+
+}
